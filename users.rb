700df57a69c8434e7b331583dcdab1e0e82f0229
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-09-19 09:44:09 +0900

    authorizationヘッダについての処理を追加

--- users.rb
+++ users.rb
@@ -1,6 +1,13 @@
 require 'webrick'
 require 'json'
 require 'pg'
+require 'base64'
+
+# auhorizationヘッダのチェック
+def check_authorization_header(encoded_str,user_id,password)
+  decoded_str = Base64.decode64(encoded_str)
+  decoded_str.eql?(user_id+":"+password)
+end
 
 server = WEBrick::HTTPServer.new({ 
   :DocumentRoot => './',
@@ -13,12 +20,25 @@ conn = PG.connect( dbname: 'code_track', user: 'postgres' )
 
 server.mount_proc '/users' do |req, res|
   # TODO: authorizationヘッダーがうんたらを書く
+  authorization_header = req.header["authorization"][0]
+  user_id = req.path.split('/')[-1]
+  result = conn.exec("SELECT * FROM users WHERE id = $1", [user_id])
+  user = result.to_a[0].map {|key,val| [key,val.rstrip]}.to_h
+  password = user["password"]
+  if check_authorization_header(authorization_header, user_id, password)
+    puts "ok"
+  else
+    puts "ng"
+  end
+
+  
+  # check_authorization_header(authorization_header,user_id,password)
   # TODO: 要求されているuser_idを返す
+  
+  puts user_id
   # TODO: 何かをする
-  req_head = req.header
-  puts req_head
-  req_body = req.body
-  req_body_h = JSON.parse(req_body)
+  
+  res.body = user.to_json
 end
 
 trap("INT"){ server.shutdown }
